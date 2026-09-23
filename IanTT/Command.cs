9cb1fd2219e2c081c1179e3e4aba26f5b4270fd7
Ian <platut@naver.com>
2026-09-23 14:38:48 +0900

    이제부터 시작이다.

---
+++ IanTT/Command.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB.Structure;
+
+namespace IanTT
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            //Reference r = uiDoc.Selection.PickObject
+            //    (ObjectType.Element, "객체를 선택하세요");
+            //Element e = doc.GetElement(r);
+
+            IList<Reference> refs = uiDoc.Selection.PickObjects
+                (ObjectType.Face, "객체들을 선택하세요");
+
+            Face face = doc.GetElement(refs[0]).
+                GetGeometryObjectFromReference(refs[0]) as Face;
+
+            List<Curve> dd = Util.GetCurvesFromFace(face);
+
+            //List<Curve> curves = new List<Curve>();
+            //foreach (Reference item in refs)
+            //{
+            //    Edge edge = doc.GetElement(item).
+            //        GetGeometryObjectFromReference(item) as Edge;
+            //    Curve c = edge.AsCurve();
+            //    curves.Add(c);
+            //}
+
+            FamilySymbol tt = Util.GetFamilySymbolByName("G1", doc);
+            if(tt == null)
+            {
+                Autodesk.Revit.UI.TaskDialog.Show("오류", "해당이름의 패밀리심볼을 찾지 못했습니다.");
+                return Result.Failed;
+            }
+            Level level = doc.ActiveView.GenLevel;
+            foreach (Curve c in dd)
+            {
+                using (Transaction trans = new Transaction(doc, "Create Beam"))
+                {
+                    trans.Start();
+                    tt.Activate();
+                    FamilyInstance fi = doc.Create.NewFamilyInstance
+                        (c, tt, level, StructuralType.Beam);
+                    trans.Commit();
+                }
+            }
+
+            //foreach (Reference r in refs)
+            //{
+            //    Element e = doc.GetElement(r);
+            //    Wall wall = e as Wall;
+            //    Parameter param = wall.get_Parameter
+            //        (BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS);
+            //    Parameter param2 = wall.LookupParameter("마크");
+
+            //    using(Transaction trans = new Transaction(doc, "Set Comments"))
+            //    {
+            //        trans.Start();
+            //        param.Set("IanTT");
+            //        //param2.Set("123");
+            //        trans.Commit();
+            //    }
+            //    string a = param.AsString();
+            //    Autodesk.Revit.UI.TaskDialog.Show("코멘트의 정보는 : ", a);
+
+            //}
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ IanTT/Util.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+
+namespace IanTT
+{
+    public class Util
+    {
+        /// <summary>
+        /// 선택한 Face의 Edge를 Curve로 반환하는 함수
+        /// </summary>
+        /// <param name="레빗에서 선택한 Face"></param>
+        /// <returns></returns>
+        public static List<Curve> GetCurvesFromFace(Face face)
+        {
+            List<Curve> curves = new List<Curve>();
+            EdgeArrayArray edgeArrays = face.EdgeLoops;
+            foreach (EdgeArray edgeArray in edgeArrays)
+            {
+                foreach (Edge edge in edgeArray)
+                {
+                    Curve c = edge.AsCurve();
+                    curves.Add(c);
+                }
+            }
+            return curves;
+        }
+
+        /// <summary>
+        /// 이름으로 패밀리심볼을 찾는다.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="doc"></param>
+        /// <returns></returns>
+        public static FamilySymbol GetFamilySymbolByName(string name, Document doc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_StructuralFraming);
+            collector.OfClass(typeof(FamilySymbol));
+            FamilySymbol fs = null;
+
+            foreach (FamilySymbol item in collector)
+            {
+                if(name == item.Name)
+                {
+                    fs = item;
+                    break;
+                }
+            }
+            return fs;
+        }
+
+
+    }
+}
